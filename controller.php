65f7dc8724cc8b2f2a791af1ed29ea2b07f9c93e
DelMundo Kristian <kristian.delmundo.iics@ust.edu.ph>
2020-01-29 17:19:48 +0800

    commit new2

--- controller.php
+++ controller.php
@@ -19,7 +19,7 @@ $errors   = array();
 
 
 
-$db = mysqli_connect('us-cdbr-iron-east-04.cleardb.net', 'b12a8f726f9b09', 'e6650e74', 'accounttest');
+$db = mysqli_connect('us-cdbr-iron-east-04.cleardb.net', 'b12a8f726f9b09', 'e6650e74', 'heroku_524f94403e88c36');
 
 if (isset($_POST['button1'])) { 
 	login();
